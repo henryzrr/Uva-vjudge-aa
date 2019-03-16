23af693fd816e5975eba13c153786542ddcc968f
henry <henry.zrz@gmail.com>
2019-03-16 17:01:07 -0400

    uva 10191 terminado

--- uva-100.c
+++ uva-100.c
@@ -4,7 +4,7 @@ int giveSecuence(unsigned long n);
 int main(){
     unsigned long a,b,menor,mayor;
     while(scanf("%lu %lu",&a,&b)!=EOF){
-        int res=0;
+        int respuesta=0;
         if(a>=b){
             mayor=a;
             menor=b;
@@ -15,20 +15,20 @@ int main(){
 
         while(menor<=mayor){
             int r=giveSecuence(menor);
-            if(r>res) res=r;
+            if(r>respuesta) respuesta=r;
             menor+=1;
         }
 
-        printf("%d %d %d\n",a,b,res);
+        printf("%d %d %d\n",a,b,respuesta);
     }
     return 0;
 }
 int giveSecuence(unsigned long n){
-    int cont=1,i;
+    int contador=1,i;
     while(n!=1){
         if(n%2!=0) n=(3*n) +1;
         else n=n/2;
-        cont++;
+        contador++;
     }
-    return cont;
+    return contador;
 }
--- uva-10191.c
+++ uva-10191.c
@@ -1,57 +1,84 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-    int cases,day=1;
-    char ls[256];
-    char h[4]={0,0,0,0};
-    char hr[4]={10,0,0,0};
-    int hini=0,hfin,hres=0;
+    int cases,day=0;
+    char ls[300];
+    int lsi[100][2];
+    int i=0,j,aux,anterior,ultimo,resp;
     
-    while(fgets(ls,256,stdin)){
+    while(fgets(ls,300,stdin)!=NULL){
        
         if(cases==0){
             cases = atoi(ls);
+            if(cases<1 || cases>100)cases=0;
+            else day++;
             continue;
         }
+        lsi[i][0]=((ls[1]-48)*60)+(((ls[3]-48)*10)+(ls[4]-48));
+        lsi[i][1]=((ls[7]-48)*60)+(((ls[9]-48)*10)+(ls[10]-48));
 
-        hfin = ((ls[1]-48)*60)+((ls[3]-48)*10)+(ls[4]-48);
-        int aux = hfin - hini;
-        
-        if(aux>hres){
-            hres=aux;
-            if(hini!=0){
-                hr[0]=h[0];
-                hr[1]=h[1];
-                hr[2]=h[2];
-                hr[3]=h[3];
-            }
-        }
+        if(i+1==cases){
+            for(j=0;j<i;j++){
+                for(aux=j+1;aux<=i;aux++){
+                    if(lsi[aux][0]<lsi[j][0]){
+                        int lsaux[2];
+                        lsaux[0] = lsi[j][0];
+                        lsaux[1] = lsi[j][1];
+                        lsi[j][0] = lsi[aux][0];
+                        lsi[j][1] = lsi[aux][1];
+                        lsi[aux][0] = lsaux[0];
+                        lsi[aux][1] = lsaux[1];
+                    }
+                }
 
-        h[0]=ls[6];
-        h[1]=ls[7];
-        h[2]=ls[9];
-        h[3]=ls[10];
-        hini = ((ls[7]-48)*60)+((ls[9]-48)*10)+(ls[10]-48);
-        cases--;
-        
-        if(cases==0){
-            int aux2 = 480 - hini;
-            if(aux2>hres){
-                 hres = aux2;
-                 hr[0]=h[0];
-                hr[1]=h[1];
-                hr[2]=h[2];
-                hr[3]=h[3];
             }
-            int horas = hres/60,minutos = hres%60;
+            for(j=0;j<i;j++){
+                for(aux=j+1;aux<=i;aux+=1){
+                    if(lsi[aux][0]<=lsi[j][1] && lsi[aux][1]<=lsi[j][1]){
+                        lsi[aux][0]=lsi[j][1];
+                        lsi[aux][1]=lsi[j][1];
+                    }else if(lsi[aux][0]<=lsi[j][1] && lsi[aux][1]>=lsi[j][1]){
+                        lsi[aux][0]=lsi[j][1];
+                    } 
+                }
+            }
+           
+            ultimo = 0;
+            resp = 0;
+            anterior = 0;
+            for(j=0;j<cases;j++){
+                aux=lsi[j][0]-anterior;
+                if(aux>resp){
+                    resp=aux;
+                    ultimo = anterior;
+                    
+                }
+                anterior = lsi[j][1];
+                
+            }
+            aux=480-anterior;
+            if(aux>resp){
+                resp=aux;
+                ultimo = anterior;
+            }
+            int horas = resp/60,minutos = resp%60;
+            int aux2 = ultimo%60;
             if(horas>0)
-                printf("Day #%i: the longest nap starts at %c%c:%c%c and will last for %i hours and %i minutes.\n",day,hr[0],hr[1],hr[2],hr[3],horas,minutos);
+                if(aux2>=10)
+                    printf("Day #%i: the longest nap starts at %i:%i and will last for %i hours and %i minutes.\n",day,(ultimo/60)+10,ultimo%60,horas,minutos);
+                else
+                    printf("Day #%i: the longest nap starts at %i:0%i and will last for %i hours and %i minutes.\n",day,(ultimo/60)+10,ultimo%60,horas,minutos);
             else
-                printf("Day #%i: the longest nap starts at %c%c:%c%c and will last for %i minutes.\n",day,hr[0],hr[1],hr[2],hr[3],minutos);
-            day++;
-            hres=0;
-            hini=0;
+                if(aux2>=10)
+                    printf("Day #%i: the longest nap starts at %i:%i and will last for %i minutes.\n",day,(ultimo/60)+10,ultimo%60,minutos);
+                else
+                    printf("Day #%i: the longest nap starts at %i:0%i and will last for %i minutes.\n",day,(ultimo/60)+10,ultimo%60,minutos);
+            cases=i=0;
+            
+        }else{
+            i++;
         }
+        
         
     }
     return 0;
