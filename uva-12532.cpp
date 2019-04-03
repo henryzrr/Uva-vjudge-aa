d422debab42ef73aeb1e4277cd4fb04b558c63ca
henry <henry.zrz@gmail.com>
2019-04-03 11:02:46 -0400

    uva 12532 accepted

--- uva-12532.cpp
+++ uva-12532.cpp
@@ -1,77 +1,84 @@
 #include<cstdio>
 #include<vector>
 using namespace std;
- int ls[100005];
-
-void updateFT(int pos,int val,vector<int>*lss,int n){
-    while(pos<=n){
-        (*lss)[pos] +=val;
-        pos += pos&-pos;
+int ls[100005];
+int lszeros[100005];
+int lsnegs[100005];
+int tam;
+void resetLs(int n){
+    for(int i=0;i<=n;i++){
+        lszeros[i]=0;
+        lsnegs[i]=0;
+        tam=n;
     }
 }
-int getProduct(int a,vector<int>* ls){
+void updateFT(int pos,int val,int lss){
+    int *aux=lss==0?lszeros:lsnegs;
+    while(pos<=tam){
+        aux[pos]+=val;
+        pos +=pos&-pos;
+    }
+}
+int consulta(int a,int lss){
+    int *aux=lss==0?lszeros:lsnegs; 
     int suma=0;
-    while(a>0){
-        suma +=(*ls)[a];
-        a -= a&-a;
+    while(a){   
+        suma += aux[a];
+        a -=a&-a;
     }
     return suma;
 }
-int getProduct(int a, int b,vector<int> ls){
-    int res=0;
-    if(a==1) res=getProduct(b,&ls);
-    else res=getProduct(b,&ls)-getProduct(a-1,&ls);
-    return res;
+int consulta(int a, int b, int lss){
+    return a==1?consulta(b,lss):consulta(b,lss)-consulta(a-1,lss);
 }
 int main(){
     int n,k,aux,aux2;
     char op;
     while(scanf("%d %d",&n,&k)!=EOF){
-         vector <int>lsneg,lzero;
-         lsneg.assign(n+1,0);
-         lzero.assign(n+1,0);
+        resetLs(n);
         for(int i=1;i<=n;i++){
             scanf("%i",&ls[i]);
-            if(ls[i]<0) updateFT(i,1,&lsneg,n);
-            else if(ls[i]==0) updateFT(i,1,&lzero,n);
+            if(ls[i]<0) updateFT(i,1,-1);
+            else if(ls[i]==0) updateFT(i,1,0);
         }
+        
         for(int i=0;i<k;i++){
             scanf("%c",&op);
             while(op!='P' && op !='C')scanf("%c",&op);
             scanf("%i %i",&aux,&aux2);
+            
             switch(op){
-                case 'C':
-                    if(ls[aux]>=0 && aux2<0){
-                        updateFT(aux,1,&lsneg,n);
-                    }else if( ls[aux]<0 && aux2>=0){
-                        updateFT(aux,1,&lsneg,n);
-                    } 
-                    if(ls[aux]!=0 && aux2==0){
-                        updateFT(aux,1,&lzero,n);
-                    }else if(aux2!=0 &&ls[aux]==0){
-                        updateFT(aux,-1,&lzero,n);
-                    }
-                    
-                    ls[aux]=aux2;
-                    
-                    break;
                 case 'P':
-                    
-                    if(getProduct(aux,aux2,lzero)){
+                    if(consulta(aux,aux2,0)){
                         printf("0");
-                    }else if(getProduct(aux,aux2,lsneg)%2==0){
-                        printf("+");
-                        
-                    }else{
+                    }else if(consulta(aux,aux2,-1)%2!=0){
                         printf("-");
+                    }else{
+                        printf("+");
                     }
-                
+                    break;
+                case 'C':
+                    if(aux2==0){
+                        if(ls[aux]!=0){
+                            updateFT(aux,1,0);
+                            if(ls[aux]<0)updateFT(aux,-1,-1);
+                        }
+                    }else if(aux2>0){
+                        if(ls[aux]==0) updateFT(aux,-1,0);
+                        else if(ls[aux]<0) updateFT(aux,-1,-1);
+                    }else{
+                        if(ls[aux]>=0){
+                            updateFT(aux,1,-1);
+                            if(ls[aux]==0) updateFT(aux,-1,0);
+                        }
+                    }
+                    ls[aux]=aux2;
                     break;
             }
         }
+        
         printf("\n");
-        //lsneg.clear();
-        //lzero.clear();
+       
     }
     return 0;
 }
