2bafb1292222922ffc0aad384cca14b953e3f440
henry <henry.zrz@gmail.com>
2019-04-06 15:52:42 -0400

    uva 11264 terminano

--- 5abril/uva-11264.cpp
+++ 5abril/uva-11264.cpp
@@ -1,19 +1,22 @@
 #include<stdio.h>
-
 using namespace std;
 
 int main(){
-    int cases,nroCoins,coin;
+    int cases,nroCoins;
     scanf("%i",&cases);
     while(cases){
         scanf("%i",&nroCoins);
-        int res=0,aux=0;
+        int res=0,aux=0,lsCoin[nroCoins],lastv;
         for(int i=0;i<nroCoins;i++){
-            scanf("%i",&coin);
-            if(coin>=aux){
-                aux = coin;
-                aux +=aux;
+            scanf("%i",&lsCoin[i]);
+        }
+        for(int i=0;i<nroCoins;i++){
+            if(lsCoin[i]>aux){
                 res++;
+                lastv = aux;
+                aux +=lsCoin[i];
+            }else{
+                aux = lastv+lsCoin[i];
             }
         }
         printf("%i\n",res);
